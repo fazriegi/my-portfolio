e2079b50637f467fe41899140daa9d11d7b24b26
fazriegi <fazriegi80@gmail.com>
2024-05-28 01:16:15 +0700

    fix portfolio's padding

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -19,11 +19,11 @@ const Portfolio = () => {
   }, []);
 
   return (
-    <section className="pl-6 mt-10 w-full flex flex-col items-center xl:items-start xl:ml-[25%]">
+    <section className="mt-10 w-full flex flex-col items-center xl:pl-6 xl:items-start xl:ml-[25%]">
       <h1 className="text-primary-text text-2xl text-center lg:text-3xl font-bold 2xl:text-4xl xl:text-left xl:ml-2">
         My Portfolio
       </h1>
-      <div className="w-full py-6 px-6 flex flex-col sm:flex-row sm:flex-wrap sm:gap-2 md:gap-3 md:justify-center lg:px-2 lg:gap-4 xl:gap-4 xl:justify-start xl:w-full">
+      <div className="w-full py-6 px-6 flex flex-col gap-2 sm:flex-row sm:flex-wrap md:gap-3 md:justify-center lg:px-2 lg:gap-4 xl:gap-4 xl:justify-start xl:w-full">
         {projects.map((element, i) => (
           <Card
             key={i}
